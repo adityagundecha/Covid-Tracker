c2e932a0e2b480d37700173c913e4689334485b5
Aditya Gundecha <adityagundecha@hotmail.com>
2020-04-19 18:42:42 +0530

    readjusting the position of elements

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { NativeSelect, FormControl } from '@material-ui/core';
+import { NativeSelect, FormControl, } from '@material-ui/core';
 import styles from './CountryPicker.module.css';
 import { fetchCountries } from '../../api';
+import InputLabel from '@material-ui/core/InputLabel';
 
 const CountryPicker = ({ handleCountryChange }) => {
     const [fetchedCountry, setFetchedCountry] = useState([]);
@@ -15,12 +16,15 @@ const CountryPicker = ({ handleCountryChange }) => {
     }, [setFetchedCountry]);
 
     return (
-        <FormControl className={styles.FormControl}>
+
+        <FormControl variant="outlined" className={styles.FormControl}>
+            <InputLabel shrink className={styles.InputLabel} >Select Country from dropdown</InputLabel>
             <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
                 <option value="">Global</option>
                 {fetchedCountry.map((country, i) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
+
     );
 }
 
